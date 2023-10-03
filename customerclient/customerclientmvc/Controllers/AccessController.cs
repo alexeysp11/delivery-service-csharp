18fa8933c1908bf96caa228c0c8c335dfb981af3
alexeysp11 <alexeyspiridon1111@gmail.com>
2023-10-04 00:10:59 +0500

    customerclientmvc: added request to auth service via http

--- customerclient/customerclientmvc/Controllers/AccessController.cs
+++ customerclient/customerclientmvc/Controllers/AccessController.cs
@@ -1,9 +1,13 @@
-using System.Security.Claims;
 using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using CustomerClientMVC.Models;
+using Cims.WorkflowLib.NetworkApis;
+using WokflowLib.Authentication.Models.NetworkParameters;
 
 namespace CustomerClientMVC.Controllers;
 
@@ -39,12 +43,25 @@ public class AccessController : Controller
                 throw new System.Exception("Fields are not filled properly");
             }
             // Make request to auth service 
-            // 
-            bool exists = true;
-            if (!exists)
+            var request = new UserCredentials
             {
+                Login = signInModel.Login,
+                Password = signInModel.Password
+            };
+            var responseStr = new HttpSender().Send("https://localhost:7252/AuthWebApi/VerifyUserCredentials", request);
+            System.Console.WriteLine("responseStr: " + responseStr);
+            var response = JsonSerializer.Deserialize<VUCResponse>(responseStr, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+            // 
+            if (response.WorkflowException != null && !string.IsNullOrWhiteSpace(response.WorkflowException.Message))
+                throw new System.Exception(response.WorkflowException.Message);
+            if (!string.IsNullOrWhiteSpace(response.ExceptionMessage))
+                throw new System.Exception(response.ExceptionMessage);
+            if (!response.IsVerified)
                 throw new System.Exception("Incorrect login or password");
-            }
+            // 
             var claims = new List<Claim>()
             {
                 new Claim(ClaimTypes.NameIdentifier, signInModel.Login),
@@ -63,7 +80,7 @@ public class AccessController : Controller
         }
         catch (System.Exception ex)
         {
-            ViewData["ValidationMessage"] = "Error occured: " + ex.Message;
+            ViewData["ValidationMessage"] = "Error: " + ex;
         }
         return View();
     }
@@ -93,16 +110,34 @@ public class AccessController : Controller
                 throw new System.Exception("Fields are not filled properly");
             }
             // Make request to add user
+            var request = new UserCredentials
+            {
+                Login = signUpModel.Login,
+                Email = signUpModel.Email,
+                PhoneNumber = signUpModel.PhoneNumber,
+                Password = signUpModel.Password
+            };
+            var responseStr = new HttpSender().Send("https://localhost:7252/AuthWebApi/AddUser", request);
+            var response = JsonSerializer.Deserialize<UserCreationResult>(responseStr, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
             // 
-            bool isOk = true;
-            if (!isOk)
-                ViewData["ValidationMessage"] = "Unable to add user: Some error message";
+            if (response.WorkflowException != null && !string.IsNullOrWhiteSpace(response.WorkflowException.Message))
+                throw new System.Exception(response.WorkflowException.Message);
+            if (!string.IsNullOrWhiteSpace(response.ExceptionMessage))
+                throw new System.Exception(response.ExceptionMessage);
+            if (response.UserExistanceBefore != null && response.UserExistanceBefore.LoginExists)
+                throw new System.Exception("User with the specified login already exists");
+            if (!response.IsUserAdded)
+                throw new System.Exception("Unable to add user");
+            // 
             ViewData["ValidationMessage"] = "User has been added successfully";
             return RedirectToAction("SignIn", "Access");
         }
         catch (System.Exception ex)
         {
-            ViewData["ValidationMessage"] = "Error occured: " + ex.Message;
+            ViewData["ValidationMessage"] = "Error: " + ex.Message;
         }
         return View();
     }
