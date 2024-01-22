1ff73edcb0b0994a88bdc8323c1caa891e6bc667
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-22 20:11:51 +0500

    edited warehouse backend grpc

---
+++ src/backend/warehouse/grpc/Services/WarehouseBackendService.cs
@@ -0,0 +1,85 @@
+using Grpc.Core;
+using grpc;
+
+namespace grpc.Services;
+
+public class WarehouseBackendService : WarehouseBackend.WarehouseBackendBase
+{
+    private readonly ILogger<WarehouseBackendService> _logger;
+    public WarehouseBackendService(ILogger<WarehouseBackendService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> PreprocessOrderRedirect(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> RequestStore2WhStart(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> RequestStore2WhRespond(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Store2WhSave(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> ConfirmStore2WhAccept(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Wh2KitchenStart(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Wh2KitchenExecute(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Kitchen2WhStart(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Kitchen2WhExecute(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
