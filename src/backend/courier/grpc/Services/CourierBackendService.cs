2acbe136553006fb15466d4754af458fed9d9d12
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-22 19:18:25 +0500

    edited courier backend grpc service

---
+++ src/backend/courier/grpc/Services/CourierBackendService.cs
@@ -0,0 +1,45 @@
+using Grpc.Core;
+using DeliveryService.Backend.Courier.Grpc;
+
+namespace DeliveryService.Backend.Courier.Grpc.Services;
+
+public class CourierBackendService : Greeter.GreeterBase
+{
+    private readonly ILogger<CourierBackendService> _logger;
+    public CourierBackendService(ILogger<CourierBackendService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> Store2WhStart(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> Store2WhExecute(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> DeliverOrderStart(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+
+    public override Task<HelloReply> DeliverOrderExecute(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
