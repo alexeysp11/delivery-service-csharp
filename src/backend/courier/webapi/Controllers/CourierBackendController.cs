b237ba63975bdfe9bfa1dd26113b5b1403a0bca4
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-22 19:24:46 +0500

    edited courier backend webapi service

--- src/backend/courier/webapi/Controllers/CourierBackendController.cs
+++ src/backend/courier/webapi/Controllers/CourierBackendController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Cims.WorkflowLib.Models.Business.BusinessDocuments;
+using DeliveryService.Backend.Courier.BL.Controllers;
 
 namespace DeliveryService.Backend.Courier.Webapi.Controllers;
 
