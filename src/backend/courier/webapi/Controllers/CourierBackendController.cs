0878e406fe450ecf86deca170f5212c0419bd41b
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-22 19:17:49 +0500

    edited courier backend grpc service

---
+++ src/backend/courier/webapi/Controllers/CourierBackendController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryService.Backend.Courier.Webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CourierBackendController : ControllerBase
+{
+    private readonly ILogger<CourierBackendController> _logger;
+    private CourierBackendControllerBL _backendController;
+
+    public CourierBackendController(
+        ILogger<CourierBackendController> logger,
+        CourierBackendControllerBL backendController)
+    {
+        _logger = logger;
+        _backendController = backendController;
+    }
+
+    [HttpPost(Name = "Store2WhStart")]
+    public string Store2WhStart(DeliveryOrder model)
+    {
+        return _backendController.Store2WhStart(model);
+    }
+
+    [HttpPost(Name = "Store2WhExecute")]
+    public string Store2WhExecute(DeliveryOrder model)
+    {
+        return _backendController.Store2WhExecute(model);
+    }
+
+    [HttpPost(Name = "DeliverOrderStart")]
+    public string DeliverOrderStart(DeliveryOrder model)
+    {
+        return _backendController.DeliverOrderStart(model);
+    }
+
+    [HttpPost(Name = "DeliverOrderExecute")]
+    public string DeliverOrderExecute(DeliveryOrder model)
+    {
+        return _backendController.DeliverOrderExecute(model);
+    }
+}
