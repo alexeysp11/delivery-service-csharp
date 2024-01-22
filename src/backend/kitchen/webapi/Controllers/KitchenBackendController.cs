fd4690f6e877879624813c2e748b6c32cc8e5db7
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-22 19:44:46 +0500

    edited kitchen backend webapi

---
+++ src/backend/kitchen/webapi/Controllers/KitchenBackendController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Cims.WorkflowLib.Models.Business.BusinessDocuments;
+using Cims.WorkflowLib.Models.Network;
+using DeliveryService.Backend.Kitchen.BL.Controllers;
+
+namespace DeliveryService.Backend.Kitchen.Webapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class KitchenBackendController : ControllerBase
+{
+    private readonly ILogger<KitchenBackendController> _logger;
+    private KitchenBackendControllerBL _backendController;
+
+    public KitchenBackendController(
+        ILogger<KitchenBackendController> logger,
+        KitchenBackendControllerBL backendController)
+    {
+        _logger = logger;
+        _backendController = backendController;
+    }
+
+    [HttpGet(Name = "PrepareMealStart")]
+    public string PrepareMealStart(DeliveryOrder model)
+    {
+        return _backendController.PrepareMealStart(model);
+    }
+
+    [HttpGet(Name = "PrepareMealExecute")]
+    public string PrepareMealExecute(DeliveryOrder model)
+    {
+        return _backendController.PrepareMealExecute(model);
+    }
+}
