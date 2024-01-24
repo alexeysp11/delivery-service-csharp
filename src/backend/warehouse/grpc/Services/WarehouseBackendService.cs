8bd57d664d4e395aa6fcf2a1e20becb2328b9937
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-24 21:23:34 +0500

    warehouse: edited api methods' params

--- src/backend/warehouse/grpc/Services/WarehouseBackendService.cs
+++ src/backend/warehouse/grpc/Services/WarehouseBackendService.cs
@@ -1,4 +1,6 @@
 using Grpc.Core;
+using Cims.WorkflowLib.Models.Business.BusinessDocuments;
+using DeliveryService.Backend.Warehouse.BL.Controllers;
 using DeliveryService.Backend.Warehouse.Grpc;
 
 namespace DeliveryService.Backend.Warehouse.Grpc.Services;
@@ -6,80 +8,121 @@ namespace DeliveryService.Backend.Warehouse.Grpc.Services;
 public class WarehouseBackendService : WarehouseBackend.WarehouseBackendBase
 {
     private readonly ILogger<WarehouseBackendService> _logger;
-    public WarehouseBackendService(ILogger<WarehouseBackendService> logger)
+    private WarehouseBackendControllerBL _backendController;
+
+    public WarehouseBackendService(
+        ILogger<WarehouseBackendService> logger,
+        WarehouseBackendControllerBL backendController)
     {
         _logger = logger;
+        _backendController = backendController;
     }
 
-    public override Task<GrpcApiReply> PreprocessOrderRedirect(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> PreprocessOrderRedirect(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.PreprocessOrderRedirect(model)
         });
     }
 
-    public override Task<GrpcApiReply> RequestStore2WhStart(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> RequestStore2WhStart(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.RequestStore2WhStart(model)
         });
     }
 
-    public override Task<GrpcApiReply> RequestStore2WhRespond(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> RequestStore2WhRespond(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.RequestStore2WhRespond(model)
         });
     }
 
-    public override Task<GrpcApiReply> Store2WhSave(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> Store2WhSave(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.Store2WhSave(model)
         });
     }
 
-    public override Task<GrpcApiReply> ConfirmStore2WhAccept(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> ConfirmStore2WhAccept(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.ConfirmStore2WhAccept(model)
         });
     }
 
-    public override Task<GrpcApiReply> Wh2KitchenStart(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> Wh2KitchenStart(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.Wh2KitchenStart(model)
         });
     }
 
-    public override Task<GrpcApiReply> Wh2KitchenExecute(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> Wh2KitchenExecute(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.Wh2KitchenExecute(model)
         });
     }
 
-    public override Task<GrpcApiReply> Kitchen2WhStart(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> Kitchen2WhStart(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.Kitchen2WhStart(model)
         });
     }
 
-    public override Task<GrpcApiReply> Kitchen2WhExecute(HelloRequest request, ServerCallContext context)
+    public override Task<GrpcApiReply> Kitchen2WhExecute(DeliveryOrderRequest request, ServerCallContext context)
     {
+        var model = new DeliveryOrder
+        {
+            Id = request.Id
+        };
         return Task.FromResult(new GrpcApiReply
         {
-            Message = "Hello " + request.Name
+            Message = _backendController.Kitchen2WhExecute(model)
         });
     }
 }
