f632c36fb41328e351c3a1ddb3bc4b87be63e287
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-29 21:07:07 +0500

    kitchen: added api methods

--- src/frontend/kitchen/mvc/Controllers/HomeController.cs
+++ src/frontend/kitchen/mvc/Controllers/HomeController.cs
@@ -1,21 +1,69 @@
 ﻿using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Cims.WorkflowLib.Models.Business.BusinessDocuments;
+using Cims.WorkflowLib.Models.Business.Customers;
 using DeliveryService.Frontend.Kitchen.Mvc.Models;
+using DeliveryService.Frontend.Kitchen.BL.Controllers;
 
 namespace DeliveryService.Frontend.Kitchen.Mvc.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private KitchenClientController _clientController;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(
+        ILogger<HomeController> logger,
+        KitchenClientController clientController)
     {
         _logger = logger;
+        _clientController = clientController;
     }
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    public IActionResult PrepareMealExecute()
+    {
+        ClaimsPrincipal claimUser = HttpContext.User;
+        if (claimUser != null && claimUser.Identity.IsAuthenticated)
+        {
+            ViewData["phone_number"] = claimUser.FindFirst(ClaimTypes.MobilePhone).Value;
+        }
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult PrepareMealExecute(DeliveryOrder model)
+    {
+        string preparingOrderMsg = string.Empty;
+        try
+        {
+            // if (model == null || string.IsNullOrWhiteSpace(model.City) || string.IsNullOrWhiteSpace(model.Address))
+            //     throw new System.Exception("Fields are not filled properly");
+            ClaimsPrincipal claimUser = HttpContext.User;
+            if (claimUser != null && claimUser.Identity.IsAuthenticated)
+            {
+                // Send request to the backend service to place the order.
+                // Get response and process it.
+                string response = _clientController.PrepareMealExecute(model);
+                // 
+                preparingOrderMsg = "The order was successfully placed";
+            }
+        }
+        catch (System.Exception ex)
+        {
+            preparingOrderMsg = "ERROR: " + ex.Message;
+        }
+        return Json(new {
+            Message = preparingOrderMsg
+        });
     }
 
     public IActionResult Privacy()
--- src/frontend/kitchen/mvc/Program.cs
+++ src/frontend/kitchen/mvc/Program.cs
@@ -1,7 +1,30 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Cims.WorkflowLib.Models.AppSettings;
+using DeliveryService.Core.Contexts;
+using DeliveryService.Frontend.Kitchen.BL.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddRazorRuntimeCompilation();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => {
+        options.LoginPath = "/Access/SignIn";
+        options.ExpireTimeSpan = System.TimeSpan.FromHours(12);
+    });
+builder.Services.AddSingleton<NetworkAppSettings>(settings => 
+        new NetworkAppSettings
+        {
+            ServerAddress = builder.Configuration["NetworkAppSettings:ServerAddress"],
+            Environment = builder.Configuration["NetworkAppSettings:Environment"]
+        }
+    );
+builder.Services.AddDbContext<DeliveringContext>(options =>
+        options.UseNpgsql("Server=127.0.0.1;Port=5432;Database=deliveryservicetest;Username=postgres;Password=postgres", 
+            b => b.MigrationsAssembly("DeliveryService.Core")));
+builder.Services.AddScoped<KitchenClientController>();
 
 var app = builder.Build();
 
