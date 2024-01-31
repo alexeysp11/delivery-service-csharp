b4faacbf5aeacebb445477a2e7092a40e22839cd
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-01-31 22:09:31 +0500

    core: added a new migration (property for product category type)

---
+++ src/core/Migrations/20240131170159_AddedProductCategoryType.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryService.Core.Migrations
+{
+    public partial class AddedProductCategoryType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductCategoryType",
+                table: "ProductCategories",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCategoryType",
+                table: "ProductCategories");
+        }
+    }
+}
