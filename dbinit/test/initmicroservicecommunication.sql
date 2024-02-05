2b8248fbc10823625bd9a9996ed72f949beac4f9
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-02-05 20:32:21 +0500

    dbinit: added test data for endpoints

---
+++ dbinit/test/initmicroservicecommunication.sql
@@ -0,0 +1,6 @@
+insert into "Endpoints" ("Id", "Name", "IpAddress", "Port", "NetworkAddress", "Status")
+values 
+    (1, 'customer backend (test)', 'localhost', 4001, 'https://localhost:4001/', 0),
+    (2, 'warehouse backend (test)', 'localhost', 4002, 'https://localhost:4002/', 0),
+    (3, 'kitchen backend (test)', 'localhost', 4003, 'https://localhost:4003/', 0),
+    (4, 'courier backend (test)', 'localhost', 4004, 'https://localhost:4004/', 0);
