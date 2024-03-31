f7222b924a252c0b5d4ad1ccf4d1b2176ca0316e
alexeysp11 <alexeyspiridon1111@gmail.com>
2024-03-31 15:16:36 +0500

    src: deleted unnecessary project folders

--- src/frontend/courier/mvc/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DeliveryService.Frontend.Courier.Mvc.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
